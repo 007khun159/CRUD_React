7867b89b7e5bb66e30d88d6ae8579fcff37ea8de
007khun159 <008khun159@gmail.com>
2023-08-20 17:11:13 +0700

    Create_Function

--- CRUD_API/src/pages/users.jsx
+++ CRUD_API/src/pages/users.jsx
@@ -5,25 +5,20 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
+// import ตัวของตาราง
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+//เรียกใช้ตัวของ Avatar ที่เป็น link
+import Avatar from '@mui/material/Avatar';
+// Link import
+import Link from '@mui/material/Link';
 
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 
 
@@ -54,17 +49,20 @@ export default function SimpleContainer() {
                 User
               </Typography>
             </Box>
-            <Button variant="contained">Create</Button>
+            <Link href="create">
+              <Button variant="contained">Create</Button>
+            </Link>
           </Box>
           <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>ID</TableCell>
+            <TableCell align="center">Avatar</TableCell>
             <TableCell align="right">First Name</TableCell>
-            <TableCell align="right">Last Name</TableCell>
-            <TableCell align="right">Username</TableCell>
-            <TableCell align="right">Avatar</TableCell>
+            <TableCell align="center">Last Name</TableCell>
+            <TableCell align="right">EW</TableCell>
+            
           </TableRow>
         </TableHead>
         <TableBody>
@@ -76,10 +74,14 @@ export default function SimpleContainer() {
               <TableCell component="th" scope="row">
                 {row.id}
               </TableCell>
+              <TableCell align="center">
+                <Box display="flex" justifyContent="center">
+                  <Avatar alt="Avatar" src={row.avatar} />
+                </Box>
+              </TableCell>
               <TableCell align="right">{row.fname}</TableCell>
-              <TableCell align="right">{row.lanem}</TableCell>
+              <TableCell align="center">{row.lname}</TableCell>
               <TableCell align="right">{row.username}</TableCell>
-              <TableCell align="right">{row.avatar}</TableCell>
             </TableRow>
           ))}
         </TableBody>
