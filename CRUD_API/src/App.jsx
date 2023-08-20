81d83d0f3ded935e9feb13d3f7fc5c7372f2b257
007khun159 <008khun159@gmail.com>
2023-08-20 17:11:22 +0700

    Create Function

--- CRUD_API/src/App.jsx
+++ CRUD_API/src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar  from './components/nav'
 import Users from './pages/users'
+import CreateUser from './pages/createuser'
 import './App.css'
 
 function App() {
-
-
   return (
-    <div>
-      <Navbar/>
-      <Users/>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="create" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
