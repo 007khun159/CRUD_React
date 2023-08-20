bcd6f289ab2136e878d1191767bc9748945044ec
007khun159 <008khun159@gmail.com>
2023-08-20 11:13:37 +0700

    Bassic_fetch

---
+++ CRUD_API/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar  from './components/nav'
+import Users from './pages/users'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Users/>
+    </div>
+  )
+}
+
+export default App
